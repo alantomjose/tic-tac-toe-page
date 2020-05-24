95947cf3f2fff86bd86c46d39c5091b34265fbd6
Alan <alan@Alans-MacBook-Pro.local>
2020-05-25 01:43:44 +0530

    website

---
+++ precache-manifest.fa6cc60e53166d0dd988630f2a1d28f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f339a029ac7ede92ccc5860ed005776c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5b8e7e821f6a672311df",
+    "url": "/static/css/main.82e9b055.chunk.css"
+  },
+  {
+    "revision": "2489a50cc9c73a43aa57",
+    "url": "/static/js/2.94590185.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.94590185.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b8e7e821f6a672311df",
+    "url": "/static/js/main.dad43159.chunk.js"
+  },
+  {
+    "revision": "6f92bc462155ad9de6a6",
+    "url": "/static/js/runtime-main.550eb164.js"
+  }
+]);
